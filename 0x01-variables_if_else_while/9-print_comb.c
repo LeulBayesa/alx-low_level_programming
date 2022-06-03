55fe4dbca71faa58f0c04718267a574272663767
LeulBayesa <leulbayesa@gmail.com>
2022-06-03 08:56:17 -0700

    Prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,7 +14,7 @@ int main(void)
 
 
 	for (i = 48; i <= 57; i++)
-	{	
+	{
 	putchar(i);
 
 	if (i == 57)
@@ -24,7 +24,7 @@ int main(void)
 
 	putchar(',');
 	putchar (' ');
-	
+
 	}
 
 	putchar('\n');
