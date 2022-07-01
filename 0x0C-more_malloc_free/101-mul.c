ba8ecb1677dfeefeaddb8c7fefd76cf1dfeb2f49
LeulBayesa <leulbayesa@gmail.com>
2022-07-01 13:13:35 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,7 +30,8 @@ int main(int argc, char *argv[])
 		}
 
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
-	printf("%lu\n", mul);
+
+		mul = atol(argv[1]) *atol(argv[2]);
+		printf("%lu\n", mul);
 return (0);
 }
