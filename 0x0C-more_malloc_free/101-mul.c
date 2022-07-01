89daa4acec8a762b8e069fdbfeb1eb2d9d6a59ab
LeulBayesa <leulbayesa@gmail.com>
2022-07-01 13:31:17 -0700

    multiplies two positive numbers Updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,34 +4,26 @@
  * main - multiplies two positive numbers
  * @argc: n arguments
  * @argv: args
- * Return: 0 (Success)
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-
-	unsigned long int mul;
-	int num1, num2;
-
-
+unsigned long mul;
+int i, j;
 	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (num1 = 1; num1 < argc; num1++)
-	{
-		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
 		}
 
 	}
-
-		mul = atol(argv[1]) * atol(argv[2]);
-		printf("%lu\n", mul);
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
 return (0);
 }
